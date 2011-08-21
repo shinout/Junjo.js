8d17f619509d1e8d5b242dcef5b6b6b65fe08a4a
SHIN Suzuki <shinout310+git@gmail.com>
2011-08-22 02:29:27 +0900

    modify mysql.junjo.js

--- test/mysql.junjo.js
+++ test/mysql.junjo.js
@@ -10,23 +10,43 @@ var Junjo = require('../Junjo');
 var DB_NAME  = data.dbname;
 var TBL_NAME = data.tblname;
 
-var jj = new Junjo({
-  nodeCallback: true
+var jj = new Junjo({ nodeCallback: true });
+
+jj(function() {
+  var num = Number(process.argv[2]);
+  if (isNaN(num)) num = 400;
+  this.shared.num = num;
 });
 
 jj(client.query).bind(client, 'use ' + DB_NAME, jj.callback);
 
-jj(client.query).bind(client, 'select * from ' + TBL_NAME + ' limit 1000,10', jj.callback)
-.label('select');
-
+jj(function() {
+  client.query('select * from ' + TBL_NAME + ' limit '+ this.shared.num + ',1', jj.callback)
+}).label('select');
 
 jj(function(err, records, f) {
+  var col = Object.keys(records[0])[1];
+  return [col, records[0][col]];
+}).after();
+
+jj(function(arr) {
+  var col = arr[0], val = arr[1];
+
+  var word = val.split(/[をがのはやで、。]/)[0];
+  console.log("searching " + word);
+  client.query('select * from ' + TBL_NAME + ' where ' + col + ' like "%'+ word +'%" limit 5', this.callback);
+}).after();
+
+
+jj(function(e, records, f) {
   console.log(records);
 }).after();
 
+
 // catcher
 jj.catchesAbove(function(e, jfn) {
-  console.log("err" + e.message);
+  console.log("err: " + e.message);
+  console.log("from : " + jfn.label());
   jj.terminate();
 });
 
