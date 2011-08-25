5c4ffadf3140b1ea7654a6d629a7b39f10db469d
SHIN Suzuki <shinout310+git@gmail.com>
2011-08-26 05:24:09 +0900

    modify memuse

--- test/memuse.js
+++ test/memuse.js
@@ -1,128 +1,39 @@
 var prevmem = 0;
 
-function memdiff() {
-  var cur = process.memoryUsage().heapUsed;
-  console.log(cur - prevmem - 2576);
-  prevmem = cur;
+function memdiff(n) {
+  n = n || 1;
+  while (n) {
+    var cur = process.memoryUsage().heapUsed;
+    console.log(cur - prevmem - 2576);
+    prevmem = cur;
+    n--;
+  }
 }
 
-memdiff();
-var fs = require('fs');
-memdiff();
+console.log('-------- load -----------');
+memdiff(15);
+console.log('-------- require Junjo -----------');
 var Junjo = require('../Junjo');
 memdiff();
-var $j = new Junjo();
-memdiff();
-var a = 1;
-memdiff();
-var b = {};
-memdiff();
-var c = "s";
-memdiff();
-var d = function(){}
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
-$j(function() {
-});
-memdiff();
+console.log('-------- require Junjo end -----------');
+memdiff(5);
+
+console.log('-------- new Junjo -----------');
+var i = 0, ar = [];
+while (i < 24) {
+  ar[i] = new Junjo();
+  memdiff();
+  i++;
+}
+var j = 0;
+
+console.log('-------- new Junjo end -----------');
+memdiff(5)
+console.log('-------- new $Fn -----------');
+
+while (j < 30) {
+  ar[0](function() {
+  });
+  memdiff();
+  j++;
+}
